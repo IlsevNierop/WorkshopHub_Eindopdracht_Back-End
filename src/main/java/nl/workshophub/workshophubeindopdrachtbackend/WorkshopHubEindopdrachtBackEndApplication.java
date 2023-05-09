3f0346041c1acabafcf0f7e597943d1fc1acd829
IlsevNierop <ilsevannierop@gmail.com>
2023-05-09 12:06:32 +0200

    setup structure folders

---
+++ src/main/java/nl/workshophub/workshophubeindopdrachtbackend/WorkshopHubEindopdrachtBackEndApplication.java
@@ -0,0 +1,13 @@
+package nl.workshophub.workshophubeindopdrachtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopHubEindopdrachtBackEndApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WorkshopHubEindopdrachtBackEndApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/workshophub/workshophubeindopdrachtbackend/WorkshopHubEindopdrachtBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.workshophub.workshophubeindopdrachtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopHubEindopdrachtBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
