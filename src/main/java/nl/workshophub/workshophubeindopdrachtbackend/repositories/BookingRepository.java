36e23d0636c11cff4fbca2cf75ab3bc18c956fcc
IlsevNierop <ilsevannierop@gmail.com>
2023-08-22 15:50:28 +0200

    remove unused imports

--- src/main/java/nl/workshophub/workshophubeindopdrachtbackend/repositories/BookingRepository.java
+++ src/main/java/nl/workshophub/workshophubeindopdrachtbackend/repositories/BookingRepository.java
@@ -3,8 +3,6 @@ package nl.workshophub.workshophubeindopdrachtbackend.repositories;
 import nl.workshophub.workshophubeindopdrachtbackend.models.Booking;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface BookingRepository extends JpaRepository<Booking, Long> {
 
 }
